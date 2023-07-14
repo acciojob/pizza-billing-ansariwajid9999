d29394fe2b6c42fdfc09aea498d9a744408a7e43
Wajid <ansariwajid9999@gmail.com>
2023-07-14 20:26:41 +0530

    code changed

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,11 +4,29 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
-    private String bill;
+    private String bill="";
+
+    private boolean isCheeseAdded=false;
+    private boolean isToppingsAdded=false;
+    private boolean isCarryBagAdded=false;
+    private boolean isBillGenerated=false;
+    private int totalAmount;
+    private int toppingsPrice=70;
+    private int NonVegtoppingsPrice=120;
+    private int cheesePrice=80;
+    private int carryBagPrice=20;
 
     public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        // your code goes here
+        this.isVeg=isVeg;
+        if(isVeg==true) {
+            price = 300;
+            bill = bill + "Veg pizza base price : " + price + "\n";
+            totalAmount+=price;
+        }else{
+            price = 400;
+            bill = bill + "Non-veg pizza base price: " + price + "\n";
+            totalAmount+=price;
+        }
     }
 
     public int getPrice(){
@@ -16,19 +34,39 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(isCheeseAdded == false){
+            isCheeseAdded=true;
+            totalAmount = totalAmount + cheesePrice;
+            bill = bill + "Extra cheese added : " + cheesePrice + "\n";
+        }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(!isToppingsAdded) {
+            isToppingsAdded = true;
+            if(isVeg) {
+                totalAmount = totalAmount + toppingsPrice;
+                bill = bill + "Extra toppings added : " + toppingsPrice + "\n";
+            }else{
+                totalAmount = totalAmount + NonVegtoppingsPrice;
+                bill = bill + "Extra toppings added : " + NonVegtoppingsPrice + "\n";
+            }
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(!isCarryBagAdded) {
+            isCarryBagAdded = true;
+            totalAmount = totalAmount + carryBagPrice;
+            bill = bill + "Extra carry bag added : " + carryBagPrice + "\n";
+        }
     }
 
     public String getBill(){
-        // your code goes here
+        if(isBillGenerated==false){
+            isBillGenerated = true;
+            bill = bill + "Total bill is equals : "+totalAmount + "\n";
+        }
         return this.bill;
     }
 }
